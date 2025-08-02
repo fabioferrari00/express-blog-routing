1b2eaa046eed16fcec7a718c37ceef77af4d68ab
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-08-02 15:06:47 +0200

    [fix] app.js

--- app.js
+++ app.js
@@ -2,3 +2,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+//dico ad express di utilizzare la cartella public
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  console.log('Server del mio blog');
+})
+
+//dico al server di rimanere in ascolto
+app.listen(port, () => {
+  console.log(`Server in ascolto alla porta ${port}`);
+})
