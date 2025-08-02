c1269572e51aab606b2cef82a3aeb48a8d29f708
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-08-02 15:29:22 +0200

    [feat] add all routes

--- routers/routerPosts.js
+++ routers/routerPosts.js
@@ -6,7 +6,32 @@ const router = express.Router();
 
 //index
 router.get('/', (req, res) => {
-  res.send("Elenco dei post")
+  res.send("Elenco dei post");
+})
+
+//show
+router.get('/:id', (req, res) => {
+  res.send(`Dettaglio del post con id:${req.params.id}`);
+})
+
+//create
+router.post('/', (req, res) => {
+  res.send("Creazione nuovo post");
+})
+
+//update
+router.put('/:id', (req, res) => {
+  res.send(`Modifica totale al post con id:${req.params.id}`);
+})
+
+//modify
+router.patch('/:id', (req, res) => {
+  res.send(`Modifica parziale al post con id:${req.params.id}`);
+})
+
+//delete
+router.delete('/:id', (req, res) => {
+  res.send(`Eliminazione del post con id:${req.params.id}`);
 })
 
 //esporto il router
